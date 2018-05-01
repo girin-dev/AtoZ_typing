dde2ac3229cfde87f048a79c0bb54b9fcfb48b95
girin-dev <kw8799@gmail.com>
2018-05-02 01:33:55 +0900

    v1.1.0

--- app.js
+++ app.js
@@ -3,25 +3,38 @@ var inputsecond = document.getElementById('second')
 var inputthird = document.getElementById('third')
 var inputfourth = document.getElementById('fourth')
 var inputfifth = document.getElementById('fifth')
+var inputsixth = document.getElementById('sixth')
 
 inputfirst.addEventListener('keyup', function (e) {
-    if (e.keyCode == 90) {
+    if (e.keyCode == 65) {
+        document.getElementById('button-start').click()
+    }
+    if(e.keyCode == 90) {
         inputsecond.focus();
-}})
-
+    }
+})
 inputsecond.addEventListener('keyup', function (e) {
     if (e.keyCode == 90) {
         inputthird.focus();
-}})
+    }
+})
 inputthird.addEventListener('keyup', function (e) {
     if (e.keyCode == 90) {
         inputfourth.focus();
-}}) 
+    }
+}) 
 inputfourth.addEventListener('keyup', function (e) {
     if (e.keyCode == 90) {
         inputfifth.focus();
-}}) 
-// inputfifth.addEventListener('keyup', function (e) {
-//     if (e.keyCode == 90) {
-//         inputsecond.focus();
-// }) 
+    }
+}) 
+inputfifth.addEventListener('keyup', function (e) {
+    if (e.keyCode == 90) {
+        inputsixth.focus();
+    }
+})
+inputsixth.addEventListener('keyup', function (e) {
+    if (e.keyCode == 90) {
+        document.getElementById('button-end').click()
+    }
+})
